7564c18c6f6732c98867d62f71538e47c97bb4b2
kyulee <kyulee1338@gmail.com>
2022-09-09 18:23:59 +0900

    feat: MediumCard 구현

---
+++ client/src/components/RecentMedium.js
@@ -0,0 +1,12 @@
+import MediumCard from './MediumCard';
+
+function RecentMedium() {
+  return (
+    <div>
+      <h1>최근 블로그 글</h1>
+      <MediumCard />
+    </div>
+  );
+}
+
+export default RecentMedium;
--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,8 +1,10 @@
 import Main from '../components/Main';
+import RecentMedium from '../components/RecentMedium';
 
 const HomePage = () => (
   <Main>
     <h2>Home page</h2>
+    <RecentMedium />
   </Main>
 );
 
