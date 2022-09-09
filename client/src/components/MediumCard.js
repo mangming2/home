db0345239a302bb9dd59724cd8240d8d5409c9fd
kyulee <kyulee1338@gmail.com>
2022-09-09 18:23:59 +0900

    feat: MediumCard 구현

---
+++ client/src/components/MediumCard.js
@@ -0,0 +1,116 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import XMLParser from 'react-xml-parser';
+import styled from 'styled-components';
+
+function MediumCard() {
+  const [info, setInfo] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const URL = '/feed/blockchain-valley';
+
+  function parseStr(dataSet) {
+    const dataArr = new XMLParser().parseFromString(dataSet).children;
+    const totalData = [];
+
+    for (let i = 9; i < dataArr[0].children.length; i++) {
+      const authorIndex = dataArr[0].children[i].children.length - 4;
+      const oneData = {};
+      const title = dataArr[0].children[i].children[0].value;
+      const author = dataArr[0].children[i].children[authorIndex].value;
+      let date = dataArr[0].children[i].children[authorIndex + 1].value;
+      const content = dataArr[0].children[i].children[authorIndex + 3].value;
+      const url = dataArr[0].children[i].children[1].value;
+      date.replace(',', '');
+      date = date.split(' ');
+      date.shift();
+      date.length = 3;
+      date = date.reverse();
+      const published = date.join(' ');
+      const newStartContent = content.indexOf('<img alt="" src="');
+      const newContent = content.substr(newStartContent + 17);
+      const imgIndex = newContent.indexOf('/>');
+      const imgSrc = newContent.substr(0, imgIndex - 2);
+      oneData.title = title.substr(0, title.length - 2);
+      oneData.author = author.substr(0, author.length - 2);
+      oneData.published = published;
+      oneData.img = imgSrc;
+      oneData.url = url;
+      totalData.push(oneData);
+    }
+    return totalData;
+  }
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setError(null);
+        setInfo(null);
+        setLoading(true);
+        const { data: dataSet } = await axios.get(URL);
+        setInfo(parseStr(dataSet));
+      } catch (e) {
+        setError(e);
+      }
+      setLoading(false);
+    };
+    fetchData();
+  }, []);
+
+  if (loading) return <div>아직 로딩중이에여..기다려주세여</div>;
+  if (error) return <div>오잉 에러가 발생해써요..🥺</div>;
+  if (!info) return null;
+  const CardBox = styled.div`
+    background-color: white;
+    width: 230px;
+    border-radius: 10px;
+    margin: 10px;
+    height: 290px;
+    cursor: pointer;
+    :hover {
+      border: 1px solid blue;
+      box-shadow: blue 0px 3px 20px;
+    }
+  `;
+  const CardImg = styled.img`
+    width: 100%;
+    height: 65%;
+    border-top-left-radius: 9px;
+    border-top-right-radius: 9px;
+  `;
+  const Detail = styled.div`
+    padding-left: 15px;
+  `;
+  const Title = styled.p`
+    font-weight: 600;
+    padding-top: 15px;
+    color: black;
+    font-size: 16px;
+  `;
+  const Author = styled.p`
+    font-size: 13px;
+    padding-top: 14px;
+    color: black;
+  `;
+  const Date = styled.p`
+    color: rgba(171, 171, 171, 1);
+    padding-top: 12px;
+    padding-bottom: 7px;
+    font-size: 12px;
+  `;
+  const cards = info.map((oneInfo, index) => (
+    <CardBox key={index} onClick={() => window.open(oneInfo.url, '_blank')}>
+      <CardImg src={oneInfo.img} alt="이미지 없음"></CardImg>
+      <Detail>
+        <Title>{oneInfo.title.length < 17 ? oneInfo.title : `${oneInfo.title.slice(0, 16)}...`}</Title>
+        <Author>{oneInfo.author}</Author>
+        <Date>{oneInfo.published}</Date>
+      </Detail>
+    </CardBox>
+  ));
+  if (info) {
+    return <div>{cards}</div>;
+  }
+}
+
+export default MediumCard;
