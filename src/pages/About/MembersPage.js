2ef8568e8efb21353090fe1e30569af78b4d43b6
test <test@test.com>
2022-10-10 19:11:17 +0900

    fest: Member page 구현

--- src/pages/About/MembersPage.js
+++ src/pages/About/MembersPage.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import styled from 'styled-components';
-import { supabase } from '../../supabase';
+// import { supabase } from '../../supabase';
 
 const generationCnt = 2;
 const generations = [];
@@ -19,100 +19,351 @@ const MembersPage = () => {
     setSelectedGenereation(event.target.id);
   };
 
-  const getNodeInfo = async (generation, role) => {
-    try {
-      const { data, error, status } = await supabase
-        .from('NODE')
-        .select(
-          `
-          node_id, 
-          name, 
-          generation,
-          role,
-          image
-        `
-        )
-        .match({ generation, role });
+  // const getNodeInfo = async (generation, role) => {
+  //   try {
+  //     const { data, error, status } = await supabase
+  //       .from('NODE')
+  //       .select(
+  //         `
+  //         node_id,
+  //         name,
+  //         generation,
+  //         role,
+  //         image
+  //       `
+  //       )
+  //       .match({ generation, role });
 
-      if (error && status !== 406) {
-        throw error;
-      }
+  //     if (error && status !== 406) {
+  //       throw error;
+  //     }
 
-      if (data) {
-        setNodeData(data);
-        console.log(data);
-      }
-    } catch (error) {
-      console.log(error);
-    } finally {
-      console.log('done');
-    }
-  };
+  //     if (data) {
+  //       setNodeData(data);
+  //       console.log(data);
+  //     }
+  //   } catch (error) {
+  //     console.log(error);
+  //   } finally {
+  //     console.log('done');
+  //   }
+  // };
 
   useEffect(() => {
-    getNodeInfo(selectedGeneration, 'dev');
+    // getNodeInfo(selectedGeneration, 'dev');
+    setNodeData(testData);
   }, []);
 
   return (
-    <div>
-      {nodeData === -1 ? (
-        <div>데이터 불러오는 것을 실패했습니다!</div>
-      ) : (
-        <div>
-          <div>데이터 불러오기 성공</div>
-          {nodeData.map((node, idx) => (
-            <div key={idx}>
-              <div>{idx}</div>
-              <div>{node.name}</div>
-              <div>{node.role}</div>
-              <img width={300} src={node.image} alt={node.name} />
-            </div>
-          ))}
-        </div>
-      )}
+    <Container>
+      <NavList>
+        {generations.map(el => (
+          <NavItem
+            key={el}
+            id={el}
+            className={el === parseInt(selectedGeneration, 10) ? 'isActive' : ''}
+            onClick={generationClickHandler}>{`${el}th`}</NavItem>
+        ))}
+      </NavList>
 
-      <section style={{ display: 'flex' }}>
-        <SideNav>
-          <NavList>
-            {generations.map(el => (
-              <NavItem
-                key={el}
-                id={el}
-                className={el === parseInt(selectedGeneration, 10) ? 'isActive' : ''}
-                onClick={generationClickHandler}>{`${el}기`}</NavItem>
-            ))}
-          </NavList>
-        </SideNav>
-        <MembersContainer></MembersContainer>
-      </section>
-    </div>
+      {nodeData === -1 ? (
+        <Loading>Loding~</Loading>
+      ) : (
+        <MembersContainer>
+          {nodeData.map((node, idx) => (
+            <Card key={idx}>
+              <Img src={node.image} alt={node.name} />
+              <NameContainer>
+                <NameTxt>{node.name}</NameTxt>
+                <InfoTxt>
+                  {node.generation}th | {node.role === 'dev' ? '개발팀' : '리서치팀'}
+                </InfoTxt>
+                {/* <InfoTxt>{node.role}</InfoTxt> */}
+              </NameContainer>
+              <ContactlTxt>jinseongbe@gmail.com</ContactlTxt>
+              <ContactlTxt>github</ContactlTxt>
+              <ContactlTxt>instagram</ContactlTxt>
+            </Card>
+          ))}
+        </MembersContainer>
+      )}
+    </Container>
   );
 };
 
-const SideNav = styled.aside`
-  flex: 1;
+const Container = styled.div`
+  margin-top: 5vh;
   display: flex;
   flex-direction: column;
-`;
-
-const NavList = styled.ul`
-  display: flex;
-  flex-direction: column;
-`;
-
-const NavItem = styled.li`
-  border-bottom: 1px solid #fff;
-  cursor: pointer;
-  height: 3rem;
-  &.isActive {
-    color: red;
-  }
+  justify-content: center;
+  align-items: center;
 `;
 
 const MembersContainer = styled.div`
-  flex: 2;
-  height: 30%;
-  width: 50%;
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Card = styled.div`
+  padding: 25px;
+  width: 300px;
+  height: 478px;
+  margin: 2rem;
+  background-color: #2e3453;
+  box-shadow: 0px 0px 35px 11px rgba(23, 74, 155, 0.4);
+  border-radius: 44px;
+  display: flex;
+  flex-direction: column;
+  align-items: left;
+`;
+
+const NameContainer = styled.div`
+  width: 251px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  margin-bottom: 25px;
+`;
+
+const NameTxt = styled.div`
+  font-style: normal;
+  font-weight: 300;
+  font-size: 30px;
+  line-height: 36px;
+`;
+
+const InfoTxt = styled.div`
+  font-style: normal;
+  font-weight: 300;
+  font-size: 20px;
+  line-height: 24px;
+  display: flex;
+  align-items: center;
+  color: #ffffff;
+`;
+
+const ContactlTxt = styled.div`
+  width: 163px;
+  height: 18px;
+  font-style: normal;
+  font-weight: 300;
+  font-size: 15px;
+  line-height: 18px;
+  display: flex;
+  align-items: center;
+  color: #ffffff;
+  margin-bottom: 13px;
+`;
+
+const Img = styled.img`
+  width: 251px;
+  height: 247px;
+  border-radius: 33px;
+  object-fit: cover;
+  margin-bottom: 25px;
+`;
+
+const Loading = styled.div`
+  margin: 10rem;
+  font-size: 3rem;
+  font-weight: bold;
+`;
+
+const NavList = styled.ul`
+  margin-top: 5vh;
+  height: 6rem;
+  width: 50vw;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+`;
+
+const NavItem = styled.li`
+  font-size: 3rem;
+  font-weight: 500;
+  cursor: pointer;
+  height: 3rem;
+  color: rgba(255, 255, 255, 0.6);
+  text-shadow: 0px 0px 50px rgba(6, 61, 215, 0.8);
+
+  &:hover {
+    color: rgba(255, 255, 255, 1);
+  }
+
+  &.isActive {
+    text-shadow: 0px 8px 43px rgba(19, 72, 224, 0.75);
+    color: #ffffff;
+  }
+
+  & + & {
+    margin-left: 2rem;
+  }
 `;
 
 export default MembersPage;
+
+const testData = [
+  {
+    node_id: 20220102,
+    name: '권규리',
+    generation: 1,
+    role: 'dev',
+    image:
+      'https://rnningjigplqydivritt.supabase.co/storage/v1/object/sign/bucket1/node_images/IMG_9551.JPG?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJidWNrZXQxL25vZGVfaW1hZ2VzL0lNR185NTUxLkpQRyIsImlhdCI6MTY2NTI0MDg5NiwiZXhwIjoxOTgwNjAwODk2fQ.574TeQ6kGRFuRRgb5pNgZX8o6Y3GiaKKA2hapS4QJtA',
+  },
+  {
+    node_id: 20220104,
+    name: '박진성',
+    generation: 1,
+    role: 'dev',
+    image:
+      'https://rnningjigplqydivritt.supabase.co/storage/v1/object/sign/bucket1/node_images/IMG_9552.JPG?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJidWNrZXQxL25vZGVfaW1hZ2VzL0lNR185NTUyLkpQRyIsImlhdCI6MTY2NTI0MTMzNCwiZXhwIjoxOTgwNjAxMzM0fQ.ythLwOQYFLSAx81_eXylI4e4vRHY3CW1YkjAg9IZ3H0',
+  },
+  {
+    node_id: 20220101,
+    name: '오민석',
+    generation: 1,
+    role: 'dev',
+    image:
+      'https://rnningjigplqydivritt.supabase.co/storage/v1/object/sign/bucket1/node_images/IMG_9553.JPG?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJidWNrZXQxL25vZGVfaW1hZ2VzL0lNR185NTUzLkpQRyIsImlhdCI6MTY2NTI0MTMzNiwiZXhwIjoxOTgwNjAxMzM2fQ.G7kyW4dIek_cdWHq6HWsdt3ZHE5Ql_fY7XvGIy672mk',
+  },
+  {
+    node_id: 20220103,
+    name: '한국일',
+    generation: 1,
+    role: 'dev',
+    image:
+      'https://rnningjigplqydivritt.supabase.co/storage/v1/object/sign/bucket1/node_images/IMG_9554.JPG?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJidWNrZXQxL25vZGVfaW1hZ2VzL0lNR185NTU0LkpQRyIsImlhdCI6MTY2NTI0MTY2NCwiZXhwIjoxOTgwNjAxNjY0fQ.ezJHzz5G8CP61jtCwgB2kghxXj2HveIs_4f2DNXcESk',
+  },
+  {
+    node_id: 20220102,
+    name: '권규리',
+    generation: 1,
+    role: 'dev',
+    image:
+      'https://rnningjigplqydivritt.supabase.co/storage/v1/object/sign/bucket1/node_images/IMG_9551.JPG?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJidWNrZXQxL25vZGVfaW1hZ2VzL0lNR185NTUxLkpQRyIsImlhdCI6MTY2NTI0MDg5NiwiZXhwIjoxOTgwNjAwODk2fQ.574TeQ6kGRFuRRgb5pNgZX8o6Y3GiaKKA2hapS4QJtA',
+  },
+  {
+    node_id: 20220104,
+    name: '박진성',
+    generation: 1,
+    role: 'dev',
+    image:
+      'https://rnningjigplqydivritt.supabase.co/storage/v1/object/sign/bucket1/node_images/IMG_9552.JPG?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJidWNrZXQxL25vZGVfaW1hZ2VzL0lNR185NTUyLkpQRyIsImlhdCI6MTY2NTI0MTMzNCwiZXhwIjoxOTgwNjAxMzM0fQ.ythLwOQYFLSAx81_eXylI4e4vRHY3CW1YkjAg9IZ3H0',
+  },
+  {
+    node_id: 20220101,
+    name: '오민석',
+    generation: 1,
+    role: 'dev',
+    image:
+      'https://rnningjigplqydivritt.supabase.co/storage/v1/object/sign/bucket1/node_images/IMG_9553.JPG?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJidWNrZXQxL25vZGVfaW1hZ2VzL0lNR185NTUzLkpQRyIsImlhdCI6MTY2NTI0MTMzNiwiZXhwIjoxOTgwNjAxMzM2fQ.G7kyW4dIek_cdWHq6HWsdt3ZHE5Ql_fY7XvGIy672mk',
+  },
+  {
+    node_id: 20220103,
+    name: '한국일',
+    generation: 1,
+    role: 'dev',
+    image:
+      'https://rnningjigplqydivritt.supabase.co/storage/v1/object/sign/bucket1/node_images/IMG_9554.JPG?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJidWNrZXQxL25vZGVfaW1hZ2VzL0lNR185NTU0LkpQRyIsImlhdCI6MTY2NTI0MTY2NCwiZXhwIjoxOTgwNjAxNjY0fQ.ezJHzz5G8CP61jtCwgB2kghxXj2HveIs_4f2DNXcESk',
+  },
+  {
+    node_id: 20220102,
+    name: '권규리',
+    generation: 1,
+    role: 'dev',
+    image:
+      'https://rnningjigplqydivritt.supabase.co/storage/v1/object/sign/bucket1/node_images/IMG_9551.JPG?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJidWNrZXQxL25vZGVfaW1hZ2VzL0lNR185NTUxLkpQRyIsImlhdCI6MTY2NTI0MDg5NiwiZXhwIjoxOTgwNjAwODk2fQ.574TeQ6kGRFuRRgb5pNgZX8o6Y3GiaKKA2hapS4QJtA',
+  },
+  {
+    node_id: 20220104,
+    name: '박진성',
+    generation: 1,
+    role: 'dev',
+    image:
+      'https://rnningjigplqydivritt.supabase.co/storage/v1/object/sign/bucket1/node_images/IMG_9552.JPG?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJidWNrZXQxL25vZGVfaW1hZ2VzL0lNR185NTUyLkpQRyIsImlhdCI6MTY2NTI0MTMzNCwiZXhwIjoxOTgwNjAxMzM0fQ.ythLwOQYFLSAx81_eXylI4e4vRHY3CW1YkjAg9IZ3H0',
+  },
+  {
+    node_id: 20220101,
+    name: '오민석',
+    generation: 1,
+    role: 'dev',
+    image:
+      'https://rnningjigplqydivritt.supabase.co/storage/v1/object/sign/bucket1/node_images/IMG_9553.JPG?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJidWNrZXQxL25vZGVfaW1hZ2VzL0lNR185NTUzLkpQRyIsImlhdCI6MTY2NTI0MTMzNiwiZXhwIjoxOTgwNjAxMzM2fQ.G7kyW4dIek_cdWHq6HWsdt3ZHE5Ql_fY7XvGIy672mk',
+  },
+  {
+    node_id: 20220103,
+    name: '한국일',
+    generation: 1,
+    role: 'dev',
+    image:
+      'https://rnningjigplqydivritt.supabase.co/storage/v1/object/sign/bucket1/node_images/IMG_9554.JPG?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJidWNrZXQxL25vZGVfaW1hZ2VzL0lNR185NTU0LkpQRyIsImlhdCI6MTY2NTI0MTY2NCwiZXhwIjoxOTgwNjAxNjY0fQ.ezJHzz5G8CP61jtCwgB2kghxXj2HveIs_4f2DNXcESk',
+  },
+  {
+    node_id: 20220102,
+    name: '권규리',
+    generation: 1,
+    role: 'dev',
+    image:
+      'https://rnningjigplqydivritt.supabase.co/storage/v1/object/sign/bucket1/node_images/IMG_9551.JPG?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJidWNrZXQxL25vZGVfaW1hZ2VzL0lNR185NTUxLkpQRyIsImlhdCI6MTY2NTI0MDg5NiwiZXhwIjoxOTgwNjAwODk2fQ.574TeQ6kGRFuRRgb5pNgZX8o6Y3GiaKKA2hapS4QJtA',
+  },
+  {
+    node_id: 20220104,
+    name: '박진성',
+    generation: 1,
+    role: 'dev',
+    image:
+      'https://rnningjigplqydivritt.supabase.co/storage/v1/object/sign/bucket1/node_images/IMG_9552.JPG?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJidWNrZXQxL25vZGVfaW1hZ2VzL0lNR185NTUyLkpQRyIsImlhdCI6MTY2NTI0MTMzNCwiZXhwIjoxOTgwNjAxMzM0fQ.ythLwOQYFLSAx81_eXylI4e4vRHY3CW1YkjAg9IZ3H0',
+  },
+  {
+    node_id: 20220101,
+    name: '오민석',
+    generation: 1,
+    role: 'dev',
+    image:
+      'https://rnningjigplqydivritt.supabase.co/storage/v1/object/sign/bucket1/node_images/IMG_9553.JPG?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJidWNrZXQxL25vZGVfaW1hZ2VzL0lNR185NTUzLkpQRyIsImlhdCI6MTY2NTI0MTMzNiwiZXhwIjoxOTgwNjAxMzM2fQ.G7kyW4dIek_cdWHq6HWsdt3ZHE5Ql_fY7XvGIy672mk',
+  },
+  {
+    node_id: 20220103,
+    name: '한국일',
+    generation: 1,
+    role: 'dev',
+    image:
+      'https://rnningjigplqydivritt.supabase.co/storage/v1/object/sign/bucket1/node_images/IMG_9554.JPG?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJidWNrZXQxL25vZGVfaW1hZ2VzL0lNR185NTU0LkpQRyIsImlhdCI6MTY2NTI0MTY2NCwiZXhwIjoxOTgwNjAxNjY0fQ.ezJHzz5G8CP61jtCwgB2kghxXj2HveIs_4f2DNXcESk',
+  },
+  {
+    node_id: 20220102,
+    name: '권규리',
+    generation: 1,
+    role: 'dev',
+    image:
+      'https://rnningjigplqydivritt.supabase.co/storage/v1/object/sign/bucket1/node_images/IMG_9551.JPG?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJidWNrZXQxL25vZGVfaW1hZ2VzL0lNR185NTUxLkpQRyIsImlhdCI6MTY2NTI0MDg5NiwiZXhwIjoxOTgwNjAwODk2fQ.574TeQ6kGRFuRRgb5pNgZX8o6Y3GiaKKA2hapS4QJtA',
+  },
+  {
+    node_id: 20220104,
+    name: '박진성',
+    generation: 1,
+    role: 'dev',
+    image:
+      'https://rnningjigplqydivritt.supabase.co/storage/v1/object/sign/bucket1/node_images/IMG_9552.JPG?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJidWNrZXQxL25vZGVfaW1hZ2VzL0lNR185NTUyLkpQRyIsImlhdCI6MTY2NTI0MTMzNCwiZXhwIjoxOTgwNjAxMzM0fQ.ythLwOQYFLSAx81_eXylI4e4vRHY3CW1YkjAg9IZ3H0',
+  },
+  {
+    node_id: 20220101,
+    name: '오민석',
+    generation: 1,
+    role: 'dev',
+    image:
+      'https://rnningjigplqydivritt.supabase.co/storage/v1/object/sign/bucket1/node_images/IMG_9553.JPG?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJidWNrZXQxL25vZGVfaW1hZ2VzL0lNR185NTUzLkpQRyIsImlhdCI6MTY2NTI0MTMzNiwiZXhwIjoxOTgwNjAxMzM2fQ.G7kyW4dIek_cdWHq6HWsdt3ZHE5Ql_fY7XvGIy672mk',
+  },
+  {
+    node_id: 20220103,
+    name: '한국일',
+    generation: 1,
+    role: 'dev',
+    image:
+      'https://rnningjigplqydivritt.supabase.co/storage/v1/object/sign/bucket1/node_images/IMG_9554.JPG?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJidWNrZXQxL25vZGVfaW1hZ2VzL0lNR185NTU0LkpQRyIsImlhdCI6MTY2NTI0MTY2NCwiZXhwIjoxOTgwNjAxNjY0fQ.ezJHzz5G8CP61jtCwgB2kghxXj2HveIs_4f2DNXcESk',
+  },
+];
